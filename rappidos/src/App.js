274d2e9ceb4023bf9c8e23b5d84bb5f8f07d8e97
nicolas803 <ext-nicolas.davila@jetsmart.com>
2022-11-21 23:35:20 -0300

    first sexy commit

---
+++ rappidos/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavbarComponent from './componentes/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavbarComponent/>
+    </div>
+  );
+}
+
+export default App;
