167c23a2e5e42e469ec7a8e199d7370703c45269
nicolas803 <ext-nicolas.davila@jetsmart.com>
2022-11-25 22:53:25 -0300

    doble click obligao

--- src/componentes/modalPagos.js
+++ src/componentes/modalPagos.js
@@ -1,7 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import axios from "axios";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMoneyBill1Wave,
@@ -9,60 +9,71 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function ModalPagos(props) {
+  const [show, setShow] = useState(false);
+
   const productosCarrito = JSON.parse(
     JSON.parse(sessionStorage.getItem("pedidos"))
   );
   async function postPedido() {
     let infoPedido = {
-      "estado": "OK",
-      'restaurante': 1,
-      'cliente': 1,
-      'delivery': 1
-    }
+      estado: "OK",
+      restaurante: 1,
+      cliente: 1,
+      delivery: 1,
+    };
 
-    let data
-    let res = await axios.post('http://localhost:8000/api/pedido/', infoPedido).then((response) => {
-      console.log(response);
-      data = response
-    })
+    let data;
+    let res = await axios
+      .post("http://localhost:8000/api/pedido/", infoPedido)
+      .then((response) => {
+        console.log(response);
+        data = response;
+      })
       .catch((error) => {
         console.log(error);
-        data = error
+        data = error;
       });
     console.log(data);
-  };
+  }
 
   async function postProdu() {
-    let data
+    let data;
     console.log("LOG PRODUCtos", productosCarrito);
     productosCarrito.map(async (productoDelCarrito) => {
-      console.log("carro", productoDelCarrito)
-      let resProd = await axios.post('http://localhost:8000/api/detalle_Pedido/', productoDelCarrito).then((response) => {
-        console.log(response);
-        data = response
-      })
+      console.log("carro", productoDelCarrito);
+      let resProd = await axios
+        .post("http://localhost:8000/api/detalle_Pedido/", productoDelCarrito)
+        .then((response) => {
+          console.log(response);
+          data = response;
+        })
         .catch((error) => {
           console.log(error);
-          data = error
+          data = error;
         });
       console.log(data);
-    })
-  };
+    });
+  }
   const pagar = () => {
-
-
     postPedido();
     postProdu();
-    console.log("pagar")
+    console.log("pagar");
     console.log("Lista de productos para el post: ", productosCarrito);
 
-
+    setShow(true);
     //En productosCarrito esta la lista de productos
     //window.location.href = "/seguimientoPedido";
-    props.onHide()
+    //props.onHide();
+  };
 
+  const funcionPostPago = () => {
+    //agregar codigo gay
 
-  }
+    //
+    setShow(false);
+    props.onHide();
+  };
+
   return (
     <Modal
       {...props}
@@ -70,26 +81,43 @@ function ModalPagos(props) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Metodo de pago
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Button onClick={pagar} method="POST">
-          <FontAwesomeIcon icon={faMoneyBill1Wave} /> Efectivo
-        </Button>
-        <br />
-        <br />
-        <Button disabled>
-          <FontAwesomeIcon icon={faCreditCard} /> WebPay
-        </Button>
-        <p>El metodo de pago WebPay estará disponible proximamente. </p>
-        <p>Por ahora prefiera el uso de efectivo.</p>
-        <p>Muchas gracias.</p>
-      </Modal.Body>
+      {!show ? (
+        <>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Metodo de pago
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Button onClick={pagar} method="POST">
+              <FontAwesomeIcon icon={faMoneyBill1Wave} /> Efectivo
+            </Button>
+            <br />
+            <br />
+            <Button disabled>
+              <FontAwesomeIcon icon={faCreditCard} /> WebPay
+            </Button>
+            <p>El metodo de pago WebPay estará disponible proximamente. </p>
+            <p>Por ahora prefiera el uso de efectivo.</p>
+            <p>Muchas gracias.</p>
+          </Modal.Body>
+        </>
+      ) : (
+        <>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Pagado</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Se págo se ha realizado correctamente.</p>
+          </Modal.Body>
+        </>
+      )}
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        {show ? (
+          <Button onClick={funcionPostPago}>Revisar estado</Button>
+        ) : (
+          <Button onClick={props.onHide}>Close</Button>
+        )}
       </Modal.Footer>
     </Modal>
   );
